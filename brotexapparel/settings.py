58b7a7db8de13841de4d36f85d3a886cc692990e
Zaeem Mohzar <zaeem246@gmail.com>
2022-09-06 10:36:30 +0500

    django authentication

--- brotexapparel/settings.py
+++ brotexapparel/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from email.message import Message
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -142,3 +143,17 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',   
+}
+
+# SMTP Configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'zaeem246@gmail.com'
+EMAIL_HOST_PASSWORD = 'dsbgdueojzxyqerq'
+EMAIL_USE_TLS = True
