824e8a801184748743d4f5e41812229ecaf9047c
Zaeem Mohzar <zaeem246@gmail.com>
2022-09-06 16:30:25 +0500

    handled cart and checkout page for the logged in users

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,15 @@
 # from tkinter import E
 # from urllib import request
 from django.shortcuts import render, redirect
+
+from carts.models import Cart, CartItem
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from carts.views import _cart_id
+import requests
 
 
 
@@ -20,6 +24,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+
 
 
 # Create your views here.
@@ -73,9 +78,63 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists  =CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # getting the product variation by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variation.all()
+                        product_variation.append(list(variation))
+
+                    # get the cart item id for the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user) 
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1,2,3,4,6]
+                    # ex_var_list = [4,6,3,5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)  # gives us position of the user found the common item
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity +=1
+                            item.user = user
+                            item.save()
+
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+                     
+
+            except:
+                pass
+
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+                
+
+            except:
+                return redirect('dashboard')
         
         else:
             messages.error(request, 'Invalid Login Credentials')
--- carts/urls.py
+++ carts/urls.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from django.urls import path
 from . import views
 
@@ -6,5 +7,7 @@ urlpatterns = [
     path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', views.remove_cart, name='remove_cart'),
     path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'),
+
+    path('checkout/', views.checkout, name='checkout'),
 
 ]
