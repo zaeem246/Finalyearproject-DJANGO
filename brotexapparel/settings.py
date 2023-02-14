051a2d29978e763d3a4e4d9461cc3819be6449e2
Zaeem Mohzar <zaeem.mohzar@beaconhousetechnology.com>
2023-02-14 10:56:38 +0500

    plain

--- brotexapparel/settings.py
+++ brotexapparel/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 
 # Application definition
 
--- build_files.sh
+++
@@ -1,5 +0,0 @@
-echo "BUILD START"
-python3.10.4 -m pip install -г requirements. txt
-python3.10.4 manage.py collectstatic --noinput --clear
-echo "BUILD END"
-                    
